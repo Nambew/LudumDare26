0cceb527f2612e78285db4235a2741539e98a6d8
Andre Lacasse <andre@jdr-virtuel.net>
2013-04-26 16:34:18 -0400

    add template html page.

---
+++ src/Tile.hx
@@ -0,0 +1,19 @@
+package ;
+import flash.display.BitmapData;
+
+/**
+ * ...
+ * @author andre
+ */
+class Tile
+{
+	private var _source:BitmapData;
+	private var _position:IntPoint;
+	
+	
+	public function new() 
+	{
+		
+	}
+	
+}
